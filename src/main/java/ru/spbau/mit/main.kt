6d41a9f22bfd2363f3baf70cfc2b1a865befb7dc
ZhNV2 <n-zhidkov@yandex.ru>
2017-10-28 19:02:02 +0300

    impl

--- src/main/java/ru/spbau/mit/main.kt
+++ src/main/java/ru/spbau/mit/main.kt
@@ -1,14 +1,100 @@
 package ru.spbau.mit
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    
-    words.add("world!")
+data class Edge(val firstVertex: Int, val secondVertex: Int)
 
-    return words.joinToString(separator = " ")
+class Solver(private val universities: List<Int>, private val edges: List<Edge>) {
+
+    private fun Boolean.toInt(): Int = if (this) 1 else 0
+
+    private val n: Int = edges.size + 1
+
+    private val isUniversity: List<Boolean>
+
+    init {
+        val isUniversityMutable: MutableList<Boolean> = MutableList(n + 1, { false })
+        universities.forEach {
+            isUniversityMutable[it] = true
+        }
+        isUniversity = isUniversityMutable.toList()
+    }
+
+    private fun dfsBuildTree(node: Int, depth: Int, used: MutableList<Boolean>,
+                             neighbours: List<MutableList<Int>>): TreeNode {
+        val children: MutableList<TreeNode> = mutableListOf()
+        var universitiesInSubTree: Int = isUniversity[node].toInt()
+        used[node] = true
+        neighbours[node]
+            .filterNot { used[it] }
+            .forEach {
+                val child: TreeNode = dfsBuildTree(it, depth + 1, used, neighbours)
+                universitiesInSubTree += child.universitiesInSubtree
+                children.add(child)
+            }
+        return TreeNode(children, depth, universitiesInSubTree, node)
+    }
+
+    private fun buildTree(): TreeNode {
+        val neighbours: List<MutableList<Int>> = List(n + 1, { mutableListOf<Int>() })
+        edges.forEach {
+            neighbours[it.firstVertex].add(it.secondVertex)
+            neighbours[it.secondVertex].add(it.firstVertex)
+        }
+        val used: MutableList<Boolean> = MutableList(n + 1, { false })
+        val root: TreeNode = dfsBuildTree(1, 0, used, neighbours)
+        (1 .. n)
+            .filterNot { used[it] }
+            .forEach { throw IllegalArgumentException("given list of edges does not describe tree") }
+        return root
+    }
+
+    private fun dfsGetLcaAnswer(root: TreeNode, toConnect: Int): Long {
+        root.children
+            .filter { it.universitiesInSubtree * 2 > universities.size }
+            .forEach {
+                val new = universities.size - toConnect - it.universitiesInSubtree
+                return new.toLong() * 2 * root.depth +
+                    dfsGetLcaAnswer(it, new + toConnect)
+            }
+        return (universities.size.toLong() - 2 * toConnect) * root.depth
+    }
+
+    private fun dfsGetSumOfDepths(root: TreeNode): Long {
+        var sum = if (isUniversity[root.index]) root.depth.toLong() else 0L
+        root.children
+            .forEach {
+                sum += dfsGetSumOfDepths(it)
+            }
+        return sum
+    }
+
+    fun solve(): Long {
+        val root: TreeNode = buildTree()
+        return dfsGetSumOfDepths(root) - dfsGetLcaAnswer(root, 0)
+    }
+
+    private class TreeNode(
+        val children: List<TreeNode>,
+        val depth: Int,
+        val universitiesInSubtree: Int,
+        val index: Int
+    )
+}
+
+fun readListInt(expectedSize: Int): List<Int> {
+    val list = readLine()!!.split(" ").map(String::toInt)
+    if (list.size != expectedSize) {
+        throw IllegalArgumentException("expected $expectedSize numbers, actually read ${list.size} numbers");
+    }
+    return list
 }
 
 fun main(args: Array<String>) {
-    println(getGreeting())
+    val (n, k) = readListInt(2)
+    val universities: List<Int> = readListInt(2 * k)
+    val edges: List<Edge> = List(n - 1, {
+        val (firstVertex, secondVertex) = readListInt(2)
+        Edge(firstVertex, secondVertex)
+    })
+    val answer = Solver(universities, edges).solve()
+    println(answer)
 }
--- src/test/java/ru/spbau/mit/test.kt
+++ src/test/java/ru/spbau/mit/test.kt
@@ -2,9 +2,30 @@ package ru.spbau.mit
 import kotlin.test.assertEquals
 import org.junit.Test
 
+
 class TestSource {
+
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun testCase0() {
+        val ans = Solver(
+            listOf(1, 5, 6, 2),
+            listOf(
+                Edge(1, 3), Edge(3, 2), Edge(4, 5),
+                Edge(3, 7), Edge(4, 3), Edge(4, 6)
+            )
+        ).solve()
+        assertEquals(6, ans)
+    }
+
+    @Test
+    fun testCase1() {
+        val ans = Solver(
+            listOf(3, 2, 1, 6, 5, 9),
+            listOf(
+                Edge(8, 9), Edge(3, 2), Edge(2, 7), Edge(3, 4),
+                Edge(7, 6), Edge(4, 5), Edge(2, 1), Edge(2, 8)
+            )
+        ).solve()
+        assertEquals(9, ans)
     }
 }
